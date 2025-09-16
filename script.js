4838c9d03b1fbbc3a6dc455f133da1135b4b4847
N.Upendhar <80301044+Upendhar10@users.noreply.github.com>
2025-09-16 17:28:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 //your JS code here. If required.
+
+
+const userForm = document.querySelector('form');
+
+userForm.addEventListener("submit", function(){
+	const text = document.getElementById('text').value;
+	const delay = document.getElementById('delay').value;
+
+	const output = document.getElementById('output');
+
+	PrintSomething(text, delay);
+	
+})
+
+function PrintSomething(text, delay){
+		setTimeout(() => {
+			output.innerText = text;
+		}, delay)
+}
+
+
